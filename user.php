1ef8b89f58c6861af5d0e768927dc7bf0ba200c8
Shruti Shende <shrutishende1997@gmail.com>
2019-11-10 10:59:10 +0530

    user registration page created

---
+++ user.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <form action="user_registation.php?age=20" method="post">
+NAME<input type="text"  name="name">
+LASTNAME:<input type="text" name="lastname">
+email<input type="text" name="email">
+password<input type="password"  name="password">
+gender<input type="radio" name="gender" value="male">
+<input type="radio" name="gender" value="female">
+<input type="radio" name="gender" value="other" >
+<input type="submit" name="submit" value="submit">
+    </form>
+</body>
+</html>
---
+++ user_registation.php
@@ -0,0 +1,6 @@
+<?php 
+print_r($_GET);
+print_r($_POST);
+echo "<br> req";
+print_r($_REQUEST);
+?>
